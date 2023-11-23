235298b418a0495b268e378f53b70d682b41e7cd
Martí Climent <marticlilop@gmail.com>
2023-11-23 18:59:20 +0100

    create utility to build msix

---
+++ src/Build MSIX from release.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+echo BEFORE RUNNING THIS COMMAND THE SOLUTION MUST BE BUILT WITH THE CONFIGURATION RELEASE x64!
+
+pause
+
+cd src
+cd Package
+cd bin
+cd x64
+del "WingetUI Widgets.msix"
+cd Release
+
+move .\Package\Images .\Images
+rmdir /Q /S Package
+cd WingetUIWidgetProvider
+
+rmdir /Q /S .\Package
+del Microsoft.WinUI.dll
+del Microsoft.InteractiveExperiences.Projection.dll
+
+cd ..
+cd ..
+
+MakeAppx.exe pack /d Release /p "WingetUI Widgets.msix"
+move "WingetUI Widgets.msix" ..\..\..\..\
+echo You may want to sign your installer now
+pause
